8f628f60f8140f93e5b5ddb67c71ebe02d4c1301
Mike G <csd4612@csd.uoc.gr>
2023-03-13 14:54:15 +0200

    Updated runner file

--- runner.py
+++ runner.py
@@ -2,20 +2,27 @@ import subprocess
 import time
 import statistics
 
-NUM_RUNS = 5
-NUM_THREADS = 4
+NUM_RUNS : int = 5
+NUM_THREADS : int = 4
 
-test_folder = "tests"
+test_folder : str = "tests"
 
-test_sets = [
+test_sets : list[str] = [
     "Email-Enron.txt",
     "facebook_combined.txt",
     "p2p-Gnutella24.txt"
 ]
 
-args_sets = [[test_folder + "/" + test, str(thread_count), "-m", "1"] for test in test_sets for thread_count in range(1, NUM_THREADS + 1)]
+args_sets : list[list] = [[test_folder + "/" + test, str(thread_count), "-m", "1"] for test in test_sets for thread_count in range(1, NUM_THREADS + 1)]
 
-results = {}
+# Checks if the executable exists, else runs make
+try:
+    subprocess.run(["./page_rank.exe"], capture_output=True) # Even if the executable exists, it will just invoke a message 
+                                                             # saying that the user needs to provide arguments
+except FileNotFoundError:
+    subprocess.run(["make"])
+
+results : dict[str, dict[str, dict[str, float]]] = {}
 for args in args_sets:
     times = []
     for i in range(NUM_RUNS):
@@ -27,8 +34,7 @@ for args in args_sets:
     std_dev = statistics.stdev(times)
     
     # Calculate average speedup
-    if args[1] == "1":
-        avg_speedup = 1
+    if args[1] == "1": avg_speedup = 1
     else:
         single_thread_time = results[f"{args[0]}"]["threads_1"]['avg_time']
         avg_speedup = single_thread_time / avg_time
@@ -37,6 +43,12 @@ for args in args_sets:
         results[f"{args[0]}"] = {}
 
     results[f"{args[0]}"][f"threads_{args[1]}"]= {'avg_time': avg_time, 'std_dev': std_dev, 'avg_speedup': avg_speedup}
+
+# before printing and saving the results, remove the prefix and suffix from the test names
+for test in results:
+    test_name = test.split("/")[-1]
+    test_name = test_name.split(".")[0]
+    results[test_name] = results.pop(test)
 
 for res in results:
     print(f"{res}: {results[res]}")
